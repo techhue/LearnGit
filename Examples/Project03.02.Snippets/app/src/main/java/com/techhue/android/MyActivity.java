624374ea93b3c89920887a999a06ca081bbf6526
Amarjit Singh <amarjitmca04@gmail.com>
2020-03-09 15:07:51 +0530

    Changes to be committed: 	modified:   app/build.gradle 	modified:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/techhue/android/MyActivity.java 	new file:   app/src/main/res/drawable-hdpi/ic_launcher.png 	new file:   app/src/main/res/drawable-hdpi/icon.png 	new file:   app/src/main/res/drawable-hdpi/logo.png 	new file:   app/src/main/res/drawable-ldpi/ic_launcher.png 	new file:   app/src/main/res/drawable-mdpi/ic_launcher.png 	new file:   app/src/main/res/layout/main.xml 	new file:   app/src/main/res/menu/menu.xml 	modified:   app/src/main/res/values/strings.xml 	modified:   build.gradle 	modified:   gradle/wrapper/gradle-wrapper.jar 	modified:   gradle/wrapper/gradle-wrapper.properties 	modified:   gradlew 	modified:   gradlew.bat 	modified:   settings.gradle

---
+++ Examples/Project03.02.Snippets/app/src/main/java/com/techhue/android/MyActivity.java
@@ -0,0 +1,26 @@
+package com.techhue.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MyActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
